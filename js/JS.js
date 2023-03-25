3697552b2b435c202c3e080a39d052d564ef15cc
Timurlan <magainvolter@gmail.com>
2023-03-25 14:20:02 +0300

    fix

--- js/JS.js
+++ js/JS.js
@@ -2,10 +2,10 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.next-btn').addEventListener('click', function() {
-    offset = offset + 1608;
+    offset = offset  + 1608;
 
     if (offset > 4824) {
-        offset = 0
+        offset = 0;
     }
 
     sliderLine.style.left = -offset + 'px';
@@ -20,3 +20,37 @@ document.querySelector('.prev-btn').addEventListener('click', function() {
 
     sliderLine.style.left = -offset + 'px';
 });
+
+
+
+
+
+
+
+
+
+
+    const sliderLineTwo = document.querySelector('.sliderTwo-line');
+
+
+
+    document.querySelector('.nextTwo-btn').addEventListener('click', function() {
+        offset = offset + 1137;
+
+        if (offset > 4548) {
+            offset = 0;
+        }
+
+        sliderLineTwo.style.left = -offset + 'px';
+    });
+    
+    document.querySelector('.prevTwo-btn').addEventListener('click', function() {
+        offset = offset - 1137;
+
+        if (offset < 1137) {
+            offset = 4548;
+        }
+
+        sliderLineTwo.style.left = -offset + 'px';
+    });
+
